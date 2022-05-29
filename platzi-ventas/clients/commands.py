d2d240800dd9cf31cd5d36f65eda0f7ff0173bcd
Matias Cucaresse <mcucaresse@gmail.com>
2022-05-29 02:52:16 -0300

    Comensamos a crear y estudiar la forma de poder crear comandos para la Terminal sin la necesidad de hacer el llamado al sistema de arranque de python

---
+++ platzi-ventas/clients/commands.py
@@ -0,0 +1,59 @@
+import click
+from clients.services import ClientsServices
+from clients.models import ClientModel
+
+
+@click.group()
+def clients():
+    """Manages the clients lifecycle"""
+    pass
+
+
+@clients.command()
+@click.option('-n', '--name',
+type=str,
+promp=True,
+help='The client name')
+@click.option('-c', '--company',
+type=str,
+promp=True,
+help='The client company')
+@click.option('-e', '--email',
+type=str,
+promp=True,
+help='The client email')
+@click.option('-p', '--position',
+type=str,
+promp=True,
+help='The client position')
+@click.pass_context
+def create(ctx, name, company, email, postition):
+    """Creates a new client"""
+    client = Client(name, company, email, position)
+    client_service = ClientsServices(ctx.obj['clients_table'])
+
+    client_service.create_client(client)
+
+
+@clients.command()
+@click.pass_context
+def list(ctx):
+    """List all clients"""
+    pass
+
+
+@clients.command()
+@click.pass_context
+def update(ctx, client_uid):
+    """Update a client"""
+    pass
+
+
+@clients.command()
+@click.pass_context
+def delete(ctx, client_id):
+    """Delete a client"""
+    pass
+
+
+all = clients
